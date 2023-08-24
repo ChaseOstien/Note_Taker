aa7d2cb872cd761fb4c2f1bdaa927d96527da96e
ChaseOstien <129995308+ChaseOstien@users.noreply.github.com>
2023-08-23 21:38:56 -0500

    updated file structure and worked on post request functionality

--- Develop/server.js
+++ Develop/server.js
@@ -3,6 +3,7 @@ const path = require("path");
 const fs = require('fs');
 const jsonData = require("./db/db.json");
 const uuid = require("./helpers/uuid");
+const { readFromFile, readAndAppend } = require('./helpers/fsUtils');
 
 const PORT = 3001;
 
@@ -19,7 +20,37 @@ app.get("/notes", (req, res) => {
     console.log(`${req.method} received to get notes page`);
 });
 
-app.get("/api/notes", (req, res) => res.json(jsonData));
+app.get("/api/notes", (req, res) => {
+    readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+});
+
+app.post("/api/notes", (req, res) => {
+    console.log(`${req.method} received to update notes`);
+
+    const { title, text } = req.body;
+
+    if (title && text) {
+
+    const newNote = {
+        title,
+        text,
+        review_id: uuid(),
+    };
+
+    const newString = JSON.stringify(newNote);
+
+    readAndAppend(newNote, "./db/db.json");
+    
+
+    const response = {
+        body: newNote,
+    };
+    
+    res.status(201).json(response);
+    } else {
+        res.status(500).json("Error in updating notes!");
+    }
+});
 
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"));

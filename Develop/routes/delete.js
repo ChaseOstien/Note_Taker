837282a16c32175ee4cb5fe0464475942d746861
ChaseOstien <129995308+ChaseOstien@users.noreply.github.com>
2023-08-27 21:15:22 -0500

    Removed dev comments

--- Develop/routes/delete.js
+++ Develop/routes/delete.js
@@ -5,12 +5,13 @@ const uuid = require("../helpers/uuid");
 const fs = require('fs');
 const path = require("path");
 
-router.delete("/api/notes/:id", (req, res) => { //This defines a delete requests made to the /api/notes/:id route when the deleteNote fetch request is called to to /api/notes/:id after clicking the delete button. 
+// This route deletes notes in the db file based on the id passed to the get request from the front end. 
+router.delete("/api/notes/:id", (req, res) => {
     console.log(`${req.method} recieved to delete note`);
-    const targetNote = req.params.id; // This line sets targetNote equal to the note id sent in the delete request.
-    readFromFile("./db/db.json").then((data) => { // This reads from the db.json file and assigns the file content to the data variable. 
-        const notes = JSON.parse(data); // This parses the json content in the file. 
-        const index = notes.findIndex((note) => note.id === targetNote); // This finds the array index of the note who's id matches the value assigned to targetNote.
+    const targetNote = req.params.id;
+    readFromFile("./db/db.json").then((data) => { 
+        const notes = JSON.parse(data); 
+        const index = notes.findIndex((note) => note.id === targetNote);
         if (index !== -1) {
             notes.splice(index, 1);
             } else {
@@ -18,13 +19,13 @@ router.delete("/api/notes/:id", (req, res) => { //This defines a delete requests
             }
         // If the value assigned to index is a valid index in the data array, it will be spliced from the array, if not, an error will be returned. 
 
-        fs.writeFile("./db/db.json", JSON.stringify(notes), "utf-8", (err) => { 
+        fs.writeFile("./db/db.json", JSON.stringify(notes), "utf-8", (err) => { // This writes the updated array back to the database file. 
             if (err) {
                 res.status(500).json("Error updating file after deleting note.")
             } else {
                 res.status(204).json("File updated after deleting.");
             }
-        }); // Once the target note has been deleted, writeFile takes the updated dta assigned to notes and converts it back to json format via stringify, then writes it back into the db.json file to update it. 
+        }); // Once the target note has been deleted, writeFile takes the updated data assigned to notes and converts it back to json format via stringify, then writes it back into the db.json file to update it. 
     });
 });
 
--- Develop/server.js
+++ Develop/server.js
@@ -2,24 +2,25 @@ const express = require("express"); //This line imports express into this projec
 const path = require("path"); // This line imports the path module into this file. This allows you to work with directories and file paths.
 const fs = require('fs'); // This imports the file system module from node, allowing you to manipulate files, indluding reading and writing. 
 const jsonData = require("./db/db.json"); // This line imports the data base file in this directory. 
-const api = require("./routes/index");
-const deleteRouter = require("./routes/delete");
+const api = require("./routes/index"); // This line imports the index.js api route
+const deleteRouter = require("./routes/delete"); // Imports the delete router. 
 const PORT = 3001; // This line assigns the server port we want to work with. 
 
-const app = express(); // This line creates a new instance of an express application in this project.  
+const app = express();  
 
-
-app.use(express.json()); // app refers to the instance of express that we created, .use() is used to set up middleware functions that will be executed on incoming HTTP requests. express.json() is a built in middleware function provided by express that is used to parse JSON data in incoming requests. It configures express to automatically parse JSON data in the request body. It will make it available as a JavaScript object on the request object. It will take data sent in a post request body, parse it and make it available at req.body. 
-app.use(express.urlencoded({ extended: true })); // This middleware is used to parse data from URL-encoded forms in incoming requests. Extended true allows you to parse complex objects. 
+// Middleware
+app.use(express.json());  
+app.use(express.urlencoded({ extended: true })); 
 app.use("/", deleteRouter);
 app.use("/api", api);
-app.use(express.static("public")); // This middleware is set up to serve static files when you access the public directory. This allows you to serve HTML, CSS and JS files such as those in the 2 send file get requests below. 
+app.use(express.static("public"));  
 
+// Routes that serve static files
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 
     console.log(`${req.method} made to get notes page`);
-}); // This section of code processes get requests made to the notes route. req contains info about the request, res contains info about the response. It calls the sendFile() method on the response, it creates an absolute path to the file that you want to serve. __dirname is a node.js variable that represents the current directory of the scripts file. Since server.js is in the Develop directory, you must then provide the path to the desired file from the root(Develop).
+}); 
 
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"));
@@ -27,6 +28,7 @@ app.get("*", (req, res) => {
     console.log(`${req.method} recieved to get start page`);
 });
 
+// Sets our server port
 app.listen(PORT, () => 
     console.log(`App listneing at http://localhost:${PORT}`)
 );
